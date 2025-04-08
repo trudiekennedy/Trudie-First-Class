4c1d602794d3c0c9a790f1bd8e3e09cac79f2c6a
Giuliano Paolini <giulianopaolini91@gmail.com>
2025-04-08 15:52:47 +0100

    test and function update

--- lib/high_value.py
+++ lib/high_value.py
@@ -1,7 +1,9 @@
 class HighValue:
+    
     def __init__(self, value_first, value_second):
         self.value_first = value_first
         self.value_second = value_second
+    # 
     def get_highest(self):
         if self.value_first > self.value_second:
             return "First value is higher"
@@ -9,6 +11,7 @@ class HighValue:
             return "Second value is higher"
         else:
             return "Values are equal"
+    
     def add(self, increase_by, selection):
         if selection == "first":
             self.value_first += increase_by
---
+++ tests/test_high_value.py
@@ -0,0 +1,21 @@
+from lib.high_value import *
+
+
+# check values are actually integers
+# check values are assigned to self
+# test if values are not integers
+# test if values are integers
+# test if values are present
+
+
+# test get_highest
+# testing scenario where first is higher
+# where second is higher
+# where both are equal
+
+# assert that first == number
+# assert that second == number
+# check that first/second + increase == correct
+# check if variables are not inputted
+
+# check get_highest again with new values
